655d68eefa93171e735323da30bba3d382e37fcf
Marvin Bosst <marvin.bosst@gmail.com>
2018-09-24 12:51:37 +0200

    Description

--- DbToDll/Class1.cs
+++ DbToDll/Class1.cs
@@ -12,10 +12,10 @@ namespace DbToDll
         public string Mat { get;  set; }
         public string DD { get;  set; }
         public string DF { get;  set; }
-        public int EC { get;  set; }
-        public int AG { get;  set; }
-        public int DL { get;  set; }
-        public int DE { get;  set; }
+        public int EC { get;  set; } //Ecole bool
+        public int AG { get;  set; } //Agence interim bool
+        public int DL { get;  set; } //droit lecture
+        public int DE { get;  set; } //droit ecriture
         public int RH { get;  set; }
         public int ADMIN { get;  set; }
     }
